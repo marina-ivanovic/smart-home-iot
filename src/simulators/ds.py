a46336fc664a4df1152c4f27c62354c497d7076a
Matijas Levang <micolevang@gmail.com>
2026-01-24 16:12:42 +0100

    [ADD] Publishing event and settings to function params

--- src/simulators/ds.py
+++ src/simulators/ds.py
@@ -5,10 +5,10 @@ def generate_values():
     while True:
         yield random.choice([True, False])
 
-def run_door_sensor_simulator(delay, callback, stop_event, name):
+def run_door_sensor_simulator(delay, callback, stop_event, name, publish_event, settings):
     for value in generate_values():
         time.sleep(delay)
         if value:
-             callback(value, name)
+             callback(value, name, publish_event, settings)
         if stop_event.is_set():
             break
