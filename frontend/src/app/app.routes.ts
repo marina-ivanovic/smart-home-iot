a64b4ce541dd9068a4425dce3710cb64b54f6892
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2026-02-22 12:30:57 +0100

    [UPDATE] Grafana display

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { TimerControlComponent } from './components/timer-control/timer-control.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: 'dashboard', component: DashboardComponent },
-    { path: 'timer-control', component: TimerControlComponent },
     { path: '**', redirectTo: '/dashboard' }
 ];
